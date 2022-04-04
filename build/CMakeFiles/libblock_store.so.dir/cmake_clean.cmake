88cf7f962072e78faa1a8c26d2f62c03e39b85e2
NickRuffini <ruffini@ksu.edu>
2022-04-04 09:22:03 -0500

    Added CMAKE file so we can run all the given tests

---
+++ build/CMakeFiles/libblock_store.so.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libblock_store.so.dir/src/block_store.c.o"
+  "liblibblock_store.so.pdb"
+  "liblibblock_store.so.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libblock_store.so.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
